1269790727108d90dcf57fca87b7a8e3d97bdd25
Akash Sharma <shara98@mcmaster.ca>
2022-01-10 13:25:06 -0500

    added Dxyn

--- src/Chip8.cpp
+++ src/Chip8.cpp
@@ -315,3 +315,41 @@ void Chip8::OP_Cxkk()
 
 	registers[Vx] = randByte(randGen) & byte;
 }
+
+//Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision.
+void Chip8::OP_Dxyn()
+{
+	uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+	uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+	uint8_t height = opcode & 0x000Fu;
+
+	// Wrap if going beyond screen boundaries
+	uint8_t xPos = registers[Vx] % 64;
+	uint8_t yPos = registers[Vy] % 32;
+
+	registers[0xF] = 0;
+
+	for (unsigned int row = 0; row < height; ++row)
+	{
+		uint8_t spriteByte = memory[index + row];
+
+		for (unsigned int col = 0; col < 8; ++col)
+		{
+			uint8_t spritePixel = spriteByte & (0x80u >> col);
+			uint32_t* screenPixel = &video[(yPos + row) * VIDEO_WIDTH + (xPos + col)];
+
+			// Sprite pixel is on
+			if (spritePixel)
+			{
+				// Screen pixel also on - collision
+				if (*screenPixel == 0xFFFFFFFF)
+				{
+					registers[0xF] = 1;
+				}
+
+				// Effectively XOR with the sprite pixel
+				*screenPixel ^= 0xFFFFFFFF;
+			}
+		}
+	}
+}
